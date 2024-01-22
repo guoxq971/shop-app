2e8b03b37ce2927e0ff5f461c8f930a3984243a0
guoxq971 <1498820625@qq.com>
2024-01-22 16:43:49 +0800

    feat: 创建tabbar和对应目录

---
+++ src/pages/cart/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <view>
+    <bmNavbar title="cart" />
+  </view>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import bmNavbar from '@/components/bm/navbar/navbar.vue';
+import bmTabbar from '@/components/bm/tabbar/tabbar.vue';
+</script>
+
+<style></style>
