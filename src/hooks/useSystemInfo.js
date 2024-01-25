53c837a393d3fb7c7fa9ca2bd73e92423fed0a89
washp <1161290212@qq.com>
2024-01-25 08:58:48 +0800

    feat:购物车

--- src/hooks/useSystemInfo.js
+++ src/hooks/useSystemInfo.js
@@ -19,14 +19,24 @@ export function useSystemInfo() {
   // 底部高度
   // const bottomHeight = systemInfo.windowHeight - menuButtonInfo.bottom;
   // 手机宽度
-  const phoneWidth = systemInfo.windowWidth;
+  let phoneWidth = systemInfo.windowWidth;
   // 手机高度
   const phoneHeight = systemInfo.windowHeight;
   // 手机型号
   const phoneModel = systemInfo.model;
   // 是否是ios状态
   const isIOS = systemInfo.platform.toLowerCase() === 'ios';
-
+  // 导航栏的高度
+  let navBarHeight = 44
+  // 标签栏的高度(底部导航栏的高度)
+  let tabBarHeight = 50
+  //h5 app mp-ali
+  // #ifndef APP || H5 || MP-ALIPAY
+  // 获取胶要的位置
+  const menuButtonInfo = uni.getMenuButtonBoundingClientRect()
+  navBarHeight =(menuButtonInfo.bottom - systemInfo.statusBarHeight)+(menuButtonInfo.top - systemInfo.statusBarHeight)
+  phoneWidth = menuButtonInfo.left
+  // #endif
   return {
     // capAndStatusHeight,
     // capHeight,
@@ -37,5 +47,7 @@ export function useSystemInfo() {
     phoneHeight,
     phoneModel,
     isIOS,
+    navBarHeight,
+    tabBarHeight
   };
 }
